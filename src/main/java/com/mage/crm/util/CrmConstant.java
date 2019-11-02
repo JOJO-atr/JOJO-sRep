362cf2f551cf0aabc1183ffa641931b84a8bb2fc
lbx <1043580949@qq.com>
2019-11-02 17:29:45 +0800

    2019.11.2-lbx-修改CrmConstant

--- src/main/java/com/mage/crm/util/CrmConstant.java
+++ src/main/java/com/mage/crm/util/CrmConstant.java
@@ -6,7 +6,7 @@ public class CrmConstant {
     public static final Integer OPS_FAILED_CODE=300;
     public static final String OPS_FAILED_MSG="操作失败!";
     public static final Integer LOGIN_FAILED_CODE=305;
-    public static final String LOGIN_FAILED_MSG="用户登陆失败!";
+    public static final String LOGIN_FAILED_MSG="用户登陆失败1!";
     public static final Integer LOGIN_NO_CODE=309;
     public static final String LOGIN_NO_MSG="用户未登录!";
 }
